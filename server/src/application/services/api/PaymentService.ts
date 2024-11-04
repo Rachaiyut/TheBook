52bf95431b667544dc79b4504124bd9c6dd42e73
CHARUT\charu <charuj44@yahoo.com>
2024-11-04 15:22:45 +0700

    feat(server/features/payment): implement use cases for webhook and checkout
    - Add use cases for handling webhook and checkout functionalities.
    - Implement PaymentService.ts to manage webhook and checkout logic.
    - Add PaymentController.js for routing webhook and checkout requests.

--- server/src/application/services/api/PaymentService.ts
+++ server/src/application/services/api/PaymentService.ts
@@ -1,22 +1,60 @@
-import StripeService from "@infrastructure/services/Stripe/StripeService";
 import { TYPES } from "@inversify/types";
 import { inject, injectable } from "inversify";
+
+// Service
+import { OrderService } from "@application/services/api/index";
+
+// Outer Service
+import StripeService from "@infrastructure/services/Stripe/StripeService";
+
+// Resitory
+import { OrderRepository } from "@infrastructure/repositories";
+
 
 @injectable()
 class PaymentService {
 
 
+    private readonly _orderService: OrderService;
     private readonly _Striprservice: StripeService;
 
 
     constructor(
-        @inject(TYPES.StripeService) StripeService: StripeService
+        @inject(TYPES.OrderService) orderService: OrderService,
+        @inject(TYPES.StripeService) StripeService: StripeService,
     ) {
+        this._orderService = orderService;
         this._Striprservice = StripeService;
     }
 
 
-    public async chcekout() {
+    public async chcekout(orderId: string): Promise<string | null> {
+
+        const orderEntity = await this._orderService.getOrder(orderId)
+ 
+        const url = await this._Striprservice.checkout(orderEntity.orderId, orderEntity.orderItems)
+
+        // Catch and Error
+        if (url === null) {
+
+        } 
+
+        return url
+    }
+
+
+    public async webhook(body: any, sig: string){
+
+        const orderId = await this._Striprservice.webhook(body, sig)
+
+        await this.handlePaymentSuccess(orderId);
+
+
+    }
+
+    public async handlePaymentSuccess(orderId: string) {
+        const update = await this._orderService.updateOrderStatus(orderId);
+
 
     }
 
--- server/src/presentation/controllers/api/PaymentController.ts
+++ server/src/presentation/controllers/api/PaymentController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import express, { Request, Response } from "express";
 
 // Inversify
 import { inject } from "inversify";
@@ -6,7 +6,7 @@ import { TYPES } from "@inversify/types";
 import { controller, httpPost, httpGet } from "inversify-express-utils";
 
 //Use-Cases
-import { Checkout } from "@application/use-cases/payment/index";
+import { Checkout, Webhook } from "@application/use-cases/payment/index";
 
 // Middlwares
 import { RolesMiddleware } from "@presentation/middlewares";
@@ -16,27 +16,46 @@ import { RolesMiddleware } from "@presentation/middlewares";
 class PaymentController {
 
 
-    private readonly _checkout: Checkout
+    private readonly _checkout: Checkout;
+    private readonly _webhook: Webhook;
 
 
     constructor(
         @inject(TYPES.Checkout) chcekout: Checkout,
+        @inject(TYPES.Webhook) webhook: Webhook
     ) {
-        this._checkout = chcekout
+        this._checkout = chcekout;
+        this._webhook = webhook;
     }
 
 
-    @httpPost("/")
-    public async chcekout(req: Request, res: Response) {
+    @httpPost("/webhook", express.raw({ type: 'application/json' }))
+    public async webhook(req: Request, res: Response) {
 
-        const url = await this._checkout.execute()
+        const payload = req.body;
+        const signature = req.headers['stripe-signature'] as string;
+
+        const result = await this._webhook.execute(payload, signature)
+
+        res.status(200).json({
+            success: true,
+            data: []
+        })
+    }
+
+
+    @httpPost("/:orderId")
+    public async chcekout(req: Request<{ orderId: string }>, res: Response) {
+
+        const orderId = req.params.orderId;
+
+        const url = await this._checkout.execute(orderId);
 
         res.status(201).json({
             success: true,
             data: url
         })
     }
-
 
 }
 
