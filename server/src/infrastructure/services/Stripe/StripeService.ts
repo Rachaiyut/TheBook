bf7c1103033a2bf0bf095bf8aa7b44962530a5f3
CHARUT\charu <charuj44@yahoo.com>
2024-11-04 15:04:04 +0700

    refactor(server/features/Stripe): improve error handling and Stripe methods
    - Introduce error types for better error management in the webhook service: InvalidEvent, InvalidSignature, UnsupportedEvent.
    - Implement webhook handling to fulfill orders upon successful Stripe events.
    - Add checkout method for processing order checkouts.

--- server/src/infrastructure/services/Stripe/StripeService.ts
+++ server/src/infrastructure/services/Stripe/StripeService.ts
@@ -1,9 +1,20 @@
+import { Request } from 'express';
+
 import { injectable } from 'inversify';
 
 import Local from '@shared/Local';
 
 import Stripe from 'stripe';
 const stripe = new Stripe(Local.config().stripeSecretKey);
+
+// DTO
+import IOrderLineItemDTO from '@application/dtos/order/IOrderLineItemDTO';
+
+// Error
+import ErrorFactory from '@domain/exceptions/ErrorFactory';
+import { SHOW_CONSTRAINTS_QUERY_SUPPORTABLE_OPTIONS } from '@sequelize/core/_non-semver-use-at-your-own-risk_/abstract-dialect/query-generator-typescript.js';
+import asyncHandler from '@shared/utils/asyncHandler';
+
 
 @injectable()
 class StripeService {
@@ -12,41 +23,74 @@ class StripeService {
     constructor() { }
 
 
-    public async chcekout() {
+    private getStripeConfig() {
+        const stripeSecretKey = Local.config().stripeSecretKey;
+        const stripeWebhook = Local.config().stripeWebhook
+
+        return {
+            stripeSecretKey,
+            stripeWebhook
+        };
+    }
+
+    public async checkout(orderId: string, orderLineItem: IOrderLineItemDTO[]) {
+
         const session = await stripe.checkout.sessions.create({
-            line_items: [
-                {
-                    price_data: {
-                        currency: 'thb',
-                        product_data: {
-                            name: 'T-shirt',
-                            images: ["http://127.0.0.1:8000/public/eye.png"]
-                        },
-                        unit_amount: 2000,
-                        tax_behavior: 'exclusive',
+            line_items: orderLineItem.map((item) => ({
+                price_data: {
+                    currency: 'thb',
+                    product_data: {
+                        name: item.bookName!,
+                        images: [item.bookImageCover!]
                     },
-                    quantity: 1,
+                    unit_amount: item.bookPrice! * 100
                 },
-                {
-                    price_data: {
-                        currency: 'thb',
-                        product_data: {
-                            name: 'Toy',
-                            images: ["http://127.0.0.1:8000/public/eye.png"]
-                        },
-                        unit_amount: 2000,
-                        tax_behavior: 'exclusive',
-                    },
-                    quantity: 1,
-                },
-            ],
+                quantity: item.quantity!
+            })),
+            metadata: {
+                orderId
+            },
             mode: 'payment',
             success_url: 'https://example.com/success',
             cancel_url: 'https://example.com/cancel',
-        })
+        });
+
 
         return session.url;
     }
+
+
+    public async webhook(payload: any, signature: string): Promise<string> {
+        
+        let event;
+        const { stripeWebhook } = this.getStripeConfig();
+    
+        try {
+            event = await stripe.webhooks.constructEventAsync(payload, signature, stripeWebhook);
+        } catch (error) {
+            throw ErrorFactory.createError("InvalidSignature", "Invalid signature or payload");
+        }
+    
+        if (!event || !event.type || !event.data) {
+            throw ErrorFactory.createError("InvalidEvent", "Webhook event structure is invalid");
+        }
+    
+        switch (event.type) {
+            case 'checkout.session.completed':
+
+                const orderId = event.data.object.metadata?.orderId;
+                
+                if (orderId) {
+                    return orderId;
+                } else {
+                    throw ErrorFactory.createError("NotFound", "Order ID not found in event metadata");
+                }
+    
+            default:
+                throw ErrorFactory.createError("UnsupportedEvent", `Unhandled event type: ${event.type}`);
+        }
+    }
+    
 
 }
 
